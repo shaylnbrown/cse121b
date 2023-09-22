5d8036ec86ffdccf8e6eec594cc74806a2e4374e
shaylnbrown <shaylnbrown@gmail.com>
2023-09-21 22:42:16 -0600

    Update

---
+++ scripts/w02-task.js
@@ -0,0 +1,33 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullname = "Shay Brown";
+let currentYear = 2023;
+let profilePicture = "images/ProfilePic.jpg";
+const nameElement=document.getElementById('name');
+
+
+
+
+/* Step 3 - Element Variables */
+
+
+
+
+
+/* Step 4 - Adding Content */
+
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
