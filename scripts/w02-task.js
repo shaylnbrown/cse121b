80f9cedaeaca02b0e35543ac3f5a331a3fe2fbac
shaylnbrown <shaylnbrown@gmail.com>
2023-09-24 18:18:50 -0600

    Week 1

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -6,33 +6,29 @@
 let fullname = "Shay Brown";
 let currentYear = 2023;
 let profilePicture = "images/ProfilePic.jpg";
+
+/* Step 3 - Element Variables */
 const nameElement=document.getElementById('name');
 const foodElement=document.getElementById('food');
 const yearElement = document.querySelector('#year');
 const imageElement = document.querySelector('img')
 
+/* Step 4 - Adding Content */
 nameElement.innerHTML=`<strong>${fullname}</strong>`;
 yearElement.textContent=currentYear;
-imageElement.setAttribute('src',profilePicture);
-imageElement.setAttribute('alt', `Progile image of ${fullname}`);
-
-/* Step 3 - Element Variables */
-
-
-
-
-
-/* Step 4 - Adding Content */
-
-
-
-
-
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullname}`);
 
 /* Step 5 - Array */
-
-
-
+let favoriteFoods = ['Homemade Carmel', 'Dark Meat Turkey', 'Mashed Potatoes', 'Nachos'];
+foodElement.textContent = favoriteFoods;
+let mostFavorite = "Chicken Fried Rice";
+favoriteFoods.push(mostFavorite);
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.shift();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.pop();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
 
 
 
