fa6704af4f8e8ec0ba34b988ad4b942dda95e262
shaylnbrown <shaylnbrown@gmail.com>
2023-10-02 21:20:25 -0600

    Week3

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -40,10 +40,10 @@ let multiplyNumbers = () =>
 {
     let num1=Number(document.querySelector('#factor1').value);
     let num2=Number(document.querySelector('#factor2').value);
+    document.querySelector('#product').value=multiply(num1, num2);
     return multiply(num1, num2);
 }
-
-document.querySelector('#product').value=multiplyNumbers();
+document.querySelector('#multiplyNumbers').addEventListener('click',multiplyNumbers);
 
 /* Open Function Use - Divide Numbers */
 function divide(number1, number2){
@@ -61,17 +61,28 @@ document.querySelector('#divideNumbers').addEventListener('click',divideNumbers)
 
 
 /* Decision Structure */
+let today = new Date();
+let thisYear=today.getFullYear();
+document.getElementById('year').textContent=thisYear;
 
 
 /* ARRAY METHODS - Functional Programming */
 /* Output Source Array */
+let numbersArray=[1,2,3,4,5,6,7,8,9,10,11,12,13];
+document.getElementById('array').textContent=numbersArray;
 
 /* Output Odds Only Array */
+document.getElementById('odds').textContent=numbersArray.filter(number => (number % 2) != 0);
 
 /* Output Evens Only Array */
+document.getElementById('evens').textContent=numbersArray.filter(number => (number % 2) == 0);
 
 /* Output Sum of Org. Array */
+document.getElementById('sumOfArray').textContent=numbersArray.reduce((sum,number) => sum + number);
 
 /* Output Multiplied by 2 Array */
+document.getElementById('multiplied').textContent=numbersArray.map(number => number * 2);
 
 /* Output Sum of Multiplied by 2 Array */
+doubledArray=numbersArray.map(number => number * 2);
+document.getElementById('sumOfMultiplied').textContent=doubledArray.reduce((sum,number) => sum + number);
